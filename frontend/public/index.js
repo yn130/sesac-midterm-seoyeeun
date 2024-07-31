12b08976a21db2452bbd94a776774d8f9e2a5336
yn130 <tjdpdms95@gmail.com>
2024-07-31 15:50:23 +0900

    작성중 2

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,10 +1,63 @@
 const form = document.getElementById("todo-form")
 const input = document.querySelector('input')
-const ul = document.querySelector('todo-list');
+// const ul = document.querySelector('todo-list');
 
 
 
-let todoListLength = dotoList.length; 
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // const delete_btn = document.querySelector('todo_delete_btn');
+    
+    function getTodos() {
+        const todoList = document.querySelector('todo-list');
+        axios({
+            method: 'get',
+            url: 'https://jsonplaceholder.typicode.com/todos',
+            dataType:"json",
+          })
+            .then(function (response) {
+              response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+            });
 
 
+    }
+})
+
+
+
+
+
+// function getJSON() {
+//     $.ajax({
+//     type:"get",
+//     url:"data.json",
+//     dataType:"json",
+//     success: function(data){
+//         console.log("통신성공");
+//         console.log(data);
+//         str = '<TR>'; 
+//             $.each(data , function(i){
+//                 str += '<TD>' + data[i].no + '</TD>
+//                        <TD>' + data[i].name + '</TD>
+//                         <TD>' + data[i].tel + '</TD>
+//                         <TD>' + data[i].addr + '</TD>' +
+//                        '<TD>' + data[i].status + '</TD>
+//                        <TD>' + data[i].comment + '</TD>
+//                        <TD>'+ data[i].date + '</TD>';
+//                 str += '</TR>';
+//             });
+//         $('.table_body').append(str);
+//     },
+//     error:function(){
+//         console.log("통신에러");
+//     }
+// })
+// }
+
+
+// let todos = [];
+
+
+// let todoist = 
 
