58e342e72c405ca6a76a37370e77e5d8d11697bc
yn130 <tjdpdms95@gmail.com>
2024-07-31 15:00:41 +0900

    작성중

---
+++ frontend/public/index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("todo-form")
+const input = document.querySelector('input')
+const ul = document.querySelector('todo-list');
+
+
+
+let todoListLength = dotoList.length; 
+
+
+
